542c530397f64a24f18717458334d7bce1560d7d
Pascal Goldschmid <pascal.goldschmid@ifr.uni-stuttgart.de>
2025-10-08 15:51:02 +0200

    updates

--- sphinx_gazebo_ws/src/anafi_control/scripts/anafi_control_waypoint_mpc_regular.py
+++ sphinx_gazebo_ws/src/anafi_control/scripts/anafi_control_waypoint_mpc_regular.py
@@ -65,7 +65,7 @@ class MPCOSQPWaypoint():
         # Params
         #mpc params
         self.dt = 0.1
-        self.N = 35
+        self.N = 30
         self.amin = -np.array([5,5,5]) #x,y - acceleration limits, should correspond to limit attitude angle, used for MPC input
         self.amax =  np.array([5,5,5]) #x,y - acceleration limits, should correspond to limit attitude angle, used for MPC input
 
@@ -74,16 +74,13 @@ class MPCOSQPWaypoint():
         self.vmin = -np.array([10,10,2]) #x,y,z - velocity limits, z-component is used for MPC input
         self.vmax =  np.array([10,10,2]) #x,y,z - velocity limits, z-component is used for MPC input
 
-        #Don't use hard constraints on the integral terms
-        self.eint_min = np.array([-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf])
-        self.eint_max = np.array([ np.inf, np.inf, np.inf, np.inf, np.inf, np.inf])
 
-                               #      x_ref    x      y_ref   y         z_ref        z         vx_ref  vx     vy_ref      vy    vz_ref      vz    x_int   y_int  z_int    vx_int  vy_int    vz_int          
-        self.weights_Q  =   np.array([ 0.0 ,   0.0,   0.0,    0.0,       0.0,        0.0,       0e1,   0e1,    0e1,       0e1,   0e1,       0e1,   0.0,     0.0,   0.0,       0,      0,     0e0])
-        self.weights_QN =   np.array([ 3.0e3 , 3.0e3, 3.0e3,  3.0e3,    3.0e3,       3.0e3,     0e2,   0e2,    0e2,       0e2,   0e2,       0e1,   8e3,   8e3,   8e3,      0e0,    0e0,   0e0])
+                               #       x         y        z         vx     vy     vz              
+        self.weights_Q  =   np.array([ 0e-5,     0e-5,   0e-5,    1e0,   1e0,   1e0])
+        self.weights_QN =   np.array([ 1.0e3,    1.0e3,  1.0e3,   5e1,   5e1,   5e1])
         
         
-        self.weight_R = 0.20
+        self.weight_R = 0.2
 
         #Solver params
         self.solver_settings = {}
@@ -94,16 +91,16 @@ class MPCOSQPWaypoint():
         self.execution_freq = 10   
 
         #Init variables
-        self.xend = np.zeros(18)
-        self.xbegin = np.ones(18)
+        self.xend = np.zeros(6)
+        self.xbegin = np.ones(6)
         return
 
     def set_script_parameters(self):
         #Waypoint
         self.waypoint = np.zeros(7)
-        self.x_desired = np.zeros(18)
+        self.x_desired = np.zeros(6)
         #Current state
-        self.x_state = np.zeros(18) # in stability axes, x_state = [x_ref,x,y_ref,y,vx_ref,vx,vy_ref,vy_x_eint.y_eint,vx_eint,vy_eint]
+        self.x_state = np.zeros(6) # in stability axes, x_state = [x_ref,x,y_ref,y,vx_ref,vx,vy_ref,vy_x_eint.y_eint,vx_eint,vy_eint]
 
         #Transformation
         #Wait a couple of seconds to give stability axes publishing node time to come up
@@ -126,36 +123,18 @@ class MPCOSQPWaypoint():
         
         self.Ad = sparse.csc_matrix(np.block([
             #Update of states x
-            [np.eye(6), self.dt*np.eye(6),np.zeros((6,6))], #update of positional states
-            [np.zeros((6,6)),np.eye(6),np.zeros((6,6))],    #update of velocity states
-            #Update of integral error states, z
-            [np.array([
-                [ 1,-1, 0, 0, 0, 0, 0, 0, 0,0,0,0,  1, 0, 0, 0, 0, 0 ], # relative position in x-direction
-                [ 0, 0, 1,-1, 0, 0, 0, 0, 0,0,0,0,   0, 1, 0, 0, 0, 0], # relative position in y-direction
-                [ 0, 0, 0, 0, 1,-1, 0, 0, 0, 0, 0, 0,  0, 0, 1, 0, 0, 0], # relative position in z-direction
-                [ 0, 0, 0, 0, 0, 0, 1,-1, 0, 0 ,0, 0, 0, 0, 0, 1, 0, 0], #relative velocity in x-direction
-                [ 0, 0, 0, 0, 0, 0, 0, 0, 1,-1 ,0, 0, 0, 0, 0, 0, 1, 0], #relative velocity in y-direction
-                [ 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0 ,1,-1, 0, 0, 0, 0, 0, 1], #relative velocity in z-direction
-            ])]
+            [np.eye(3), self.dt*np.eye(3)], #update of positional states
+            [np.zeros((3,3)),np.eye(3)],    #update of velocity states
         ]))
         self.Bd = sparse.csc_matrix(np.block([
             [np.array([
-                [0,0,0],            #update for x_ref
                 [self.dt**2/2,0,0], #update for x. Input is an acceleration
-                [0,0,0],            #update for y_ref
                 [0,self.dt**2/2,0], #update for y. Input is an acceleration
-                [0,0,0],            #update for z_ref
                 [0,0,self.dt**2/2],      #update for z. Input is a velocity
-                [0,0,0],            #update for vx_ref 
                 [self.dt,0,0],      #update for vx. Input is an acceleration
-                [0,0,0],            #update for vy_ref 
                 [0,self.dt,0],      #update for vy. Input is an acceleration
-                [0,0,0],            #update for vz_ref 
-                [0,0,  self.dt],            #update for vz. Input is a velocity
-            ])],
-            [np.zeros((6,3))] #update for integration states
-
-
+                [0,0,  self.dt]            #update for vz. Input is a velocity
+            ])]
             ]))
 
 
@@ -165,26 +144,13 @@ class MPCOSQPWaypoint():
 
 
         #Define reference state trajectory that needs to be tracked
-        self.xr = np.ones((18,self.N+1))
+        self.xr = np.ones((6,self.N+1))
         self.xr[0,:] = self.xend[0] * self.xr[0,:]
         self.xr[1,:] = self.xend[1] * self.xr[1,:]
         self.xr[2,:] = self.xend[2] * self.xr[2,:]
         self.xr[3,:] = self.xend[3] * self.xr[3,:]
         self.xr[4,:] = self.xend[4] * self.xr[4,:]
         self.xr[5,:] = self.xend[5] * self.xr[5,:]
-        self.xr[6,:] = self.xend[6] * self.xr[6,:]
-        self.xr[7,:] = self.xend[7] * self.xr[7,:]
-        self.xr[8,:] = self.xend[8] * self.xr[8,:]
-        self.xr[9,:] = self.xend[9] * self.xr[9,:]
-        self.xr[10,:] = self.xend[10] * self.xr[10,:]
-        self.xr[11,:] = self.xend[11] * self.xr[11,:]
-        self.xr[12,:] = 0            * self.xr[12,:]   #x int state should become 0
-        self.xr[13,:] = 0            * self.xr[13,:]   #y int state should become 0
-        self.xr[14,:] = 0            * self.xr[14,:]   #z int state should become 0
-        self.xr[15,:] = 0            * self.xr[15,:]  #vx int state should become 0
-        self.xr[16,:] = 0            * self.xr[16,:]  #vy int state should become 0
-        self.xr[17,:] = 0            * self.xr[17,:]  #vz int state should become 0
-
         self.x0 = self.xbegin
 
 
@@ -195,9 +161,9 @@ class MPCOSQPWaypoint():
         #Combine umins with external force
         umins = np.tile(self.umin,(self.N,1))
         umaxs = np.tile(self.umax,(self.N,1))
-        #                       x_ref     x    y_ref     y     z_ref      z    vx_ref   vx          vy_ref   vy          vz_ref     vz             x_eint          y_eint          z_eint       vx_int          vy_int              vz_int
-        self.xmin = np.array([-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,self.vmin[0],-np.inf,self.vmin[1],-np.inf,self.vmin[2],self.eint_min[0],self.eint_min[1],self.eint_min[2],self.eint_min[3],self.eint_min[4],self.eint_min[5]])
-        self.xmax = np.array([ np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf,self.vmax[0], np.inf,self.vmax[1], np.inf,self.vmax[2],self.eint_max[0],self.eint_max[1],self.eint_max[2],self.eint_max[3],self.eint_max[4],self.eint_max[5]])
+        #                         x      y        z     vx           vy             vz      
+        self.xmin = np.array([-np.inf,-np.inf,-np.inf,self.vmin[0],self.vmin[1],self.vmin[2]])
+        self.xmax = np.array([ np.inf, np.inf, np.inf,self.vmax[0],self.vmax[1],self.vmax[2]])
 
 
         # Objective function
@@ -215,6 +181,7 @@ class MPCOSQPWaypoint():
                     np.zeros(self.N*self.nu)])
         
         print("np.shape(self.q)",np.shape(self.q))
+        print("np.shape(self.Ad)",self.Ad)
         # - linear dynamics
         self.Ax = sparse.kron(sparse.eye(self.N+1),-sparse.eye(self.nx)) + sparse.kron(sparse.eye(self.N+1, k=-1), self.Ad)
         self.Bu = sparse.kron(sparse.vstack([sparse.csc_matrix((1, self.N)), sparse.eye(self.N)]), self.Bd)
@@ -298,17 +265,11 @@ class MPCOSQPWaypoint():
     def update_x0(self):
         #To avoid invalid initialization values when copter gets to fast
         x0 = self.x_state
-        x0[7]  = np.clip(x0[7],self.vmin[0],self.vmax[0])
-        x0[9]  = np.clip(x0[9],self.vmin[1],self.vmax[1])
-        x0[11] = np.clip(x0[11],self.vmin[2],self.vmax[2])
-        #Initialize the integral error with the current error
-        x0[12] = np.clip(self.x0[12],self.eint_min[0],self.eint_max[0])
-        x0[13] = np.clip(self.x0[13],self.eint_min[1],self.eint_max[1])
-        x0[14] = np.clip(self.x0[14],self.eint_min[2],self.eint_max[2])
-        x0[15] = np.clip(self.x0[15],self.eint_min[3],self.eint_max[3])
-        x0[16] = np.clip(self.x0[16],self.eint_min[4],self.eint_max[4])
-        x0[17] = np.clip(self.x0[17],self.eint_min[5],self.eint_max[5])
         self.x0 = x0
+        #Make sure that x0 is inside value range of allowed velocities. Positional states are not constrained (i.e. value range allows +-inf)
+        self.x0[3] = np.clip(self.x0[3],self.xmin[3],self.xmax[3])
+        self.x0[4] = np.clip(self.x0[4],self.xmin[4],self.xmax[4])
+        self.x0[5] = np.clip(self.x0[5],self.xmin[5],self.xmax[5])
         return
 
     def update_mpc_problem(self):
@@ -324,28 +285,15 @@ class MPCOSQPWaypoint():
         P_update = sparse.block_diag([sparse.kron(sparse.eye(self.N), Q_update), QN_update,
                             sparse.kron(sparse.eye(self.N), self.R)], format='csc')
         
-        xr = np.ones((18,self.N+1))
+        xr = np.ones((6,self.N+1))
 
         #Tracked position is updated with constant velocity model over time horizon
-        xr[0,:] = self.x_desired[0] + np.arange(self.N+1)*self.dt*self.x_desired[6]
-        xr[1,:] = self.x_desired[0] + np.arange(self.N+1)*self.dt*self.x_desired[6]
-        xr[2,:] = self.x_desired[2] + np.arange(self.N+1)*self.dt*self.x_desired[8]
-        xr[3,:] = self.x_desired[2] + np.arange(self.N+1)*self.dt*self.x_desired[8]
-        xr[4,:] = self.x_desired[4] + np.arange(self.N+1)*self.dt*self.x_desired[10]
-        xr[5,:] = self.x_desired[4] + np.arange(self.N+1)*self.dt*self.x_desired[10]
-
-        xr[6,:] =  self.x_desired[6] #vx_ref
-        xr[7,:] =  self.x_desired[7]
-        xr[8,:] =  self.x_desired[8] #vy_ref
-        xr[9,:] =  self.x_desired[9]
-        xr[10,:] =  self.x_desired[10] #vz_ref
-        xr[11,:] =  self.x_desired[11]
-        xr[12,:] =  self.x_desired[12] #integral error state x  should be set to 0
-        xr[13,:] =  self.x_desired[13] #integral error state y  should be set to 0
-        xr[14,:] =  self.x_desired[14] #integral error state z  should be set to 0
-        xr[15,:] =  self.x_desired[15] #integral error state vx should be set to 0
-        xr[16,:] =  self.x_desired[16] #integral error state vy should be set to 0
-        xr[17,:] =  self.x_desired[17] #integral error state vz should be set to 0        
+        xr[0,:] = self.x_desired[0] + np.arange(self.N+1)*self.dt*self.x_desired[3]
+        xr[1,:] = self.x_desired[1] + np.arange(self.N+1)*self.dt*self.x_desired[4]
+        xr[2,:] = self.x_desired[2] + np.arange(self.N+1)*self.dt*self.x_desired[5]
+        xr[3,:] = self.x_desired[3]
+        xr[4,:] = self.x_desired[4]
+        xr[5,:] = self.x_desired[5]
 
         
         q_update = np.hstack([-Q_update.dot(xr[:,:self.N]).flatten(), -QN_update.dot(xr[:,self.N]),
@@ -364,12 +312,12 @@ class MPCOSQPWaypoint():
         return
 
     def read_uav_state(self,msg):
-        self.x_state[1]  =  msg.pose.pose.position.x
-        self.x_state[3]  =  msg.pose.pose.position.y
-        self.x_state[5]  =  msg.pose.pose.position.z
-        self.x_state[7]  =  msg.twist.twist.linear.vector.x
-        self.x_state[9]  =  msg.twist.twist.linear.vector.y
-        self.x_state[11] =  msg.twist.twist.linear.vector.z
+        self.x_state[0]  =  msg.pose.pose.position.x
+        self.x_state[1]  =  msg.pose.pose.position.y
+        self.x_state[2]  =  msg.pose.pose.position.z
+        self.x_state[3]  =  msg.twist.twist.linear.vector.x
+        self.x_state[4]  =  msg.twist.twist.linear.vector.y
+        self.x_state[5] =  msg.twist.twist.linear.vector.z
         q = np.array([msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w])
         roll,pitch,yaw = euler_from_quaternion([q[0],q[1],q[2],q[3]])
         self.yaw_control_state = yaw
@@ -377,25 +325,11 @@ class MPCOSQPWaypoint():
         msg_pos_error = Vector3Stamped()
         msg_pos_error.header.stamp = rospy.Time.now()
         msg_pos_error.header.frame_id = "world"
-        msg_pos_error.vector = Vector3(self.x_state[0]-self.x_state[1],self.x_state[2]-self.x_state[3],self.x_state[3]-self.x_state[4])
-        self.position_error_debug_publisher.publish(msg_pos_error)
         return
 
     
     def read_waypoint(self,msg):
-        self.x_state[0] = msg.x    #update the reference state which is to be tracked  
-        self.x_state[2] = msg.y    #update the reference state which is to be tracked  
-        self.x_state[4] = msg.z    #update the reference state which is to be tracked  
-        self.x_state[6] = msg.v_x   #update the reference state which is to be tracked 
-        self.x_state[8] = msg.v_y   #update the reference state which is to be tracked 
-        self.x_state[10] = msg.v_z  #update the reference state which is to be tracked
-        self.x_state[12] = 0
-        self.x_state[13] = 0
-        self.x_state[14] = 0
-        self.x_state[15] = 0
-        self.x_state[16] = 0
-        self.x_state[17] = 0
-
+        
         self.waypoint[0] = msg.x
         self.waypoint[1] = msg.y
         self.waypoint[2] = msg.z
@@ -404,25 +338,14 @@ class MPCOSQPWaypoint():
         self.waypoint[5] = msg.v_z
         self.waypoint[6] = msg.yaw
 
-        self.x_desired = np.zeros(18)
+        self.x_desired = np.zeros(6)
         self.x_desired[0] = self.waypoint[0]
-        self.x_desired[1] = self.waypoint[0]
-        self.x_desired[2] = self.waypoint[1]
-        self.x_desired[3] = self.waypoint[1]
-        self.x_desired[4] = self.waypoint[2]
-        self.x_desired[5] = self.waypoint[2]
-        self.x_desired[6] = self.waypoint[3]
-        self.x_desired[7] = self.waypoint[3]
-        self.x_desired[8] = self.waypoint[4]
-        self.x_desired[9] = self.waypoint[4]
-        self.x_desired[10] = self.waypoint[5]
-        self.x_desired[11] = self.waypoint[5]
-        self.x_desired[12] = 0
-        self.x_desired[13] = 0
-        self.x_desired[14] = 0
-        self.x_desired[15] = 0
-        self.x_desired[16] = 0
-        self.x_desired[17] = 0
+        self.x_desired[1] = self.waypoint[1]
+        self.x_desired[2] = self.waypoint[2]
+        self.x_desired[3] = self.waypoint[3]
+        self.x_desired[4] = self.waypoint[4]
+        self.x_desired[5] = self.waypoint[5]
+
         return
     
     def read_vz_control_effort(self,msg):
@@ -492,7 +415,7 @@ if __name__ == '__main__':
             debug.update_mpc_problem()
             debug.publish_rpyt()
         else:
-            print("\033[93mCould not find solution")
+            print("\033[93mCould not find solution\033[0m")
             debug.update_mpc_problem()
             debug.rpyt_publisher.publish(PilotingCommand())
 
